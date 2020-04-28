a7dfd7aa9c37a871f0eb34173556db728acb5b79
tianshaojian <tianshaojian@autohome.com.cn>
2020-04-28 23:09:21 +0800

    modify dir

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"github.com/jianshao/parseJSON/parse"
+)
+
+func main()  {
+	json := parse.NewParseJSON("../test/valid/test.json")
+	err := json.Load()
+	fmt.Println(err)
+}
